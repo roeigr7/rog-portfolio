3a05af00b0188149c3888bb1f89450c1891a2f8e
Roei Grinshpan <roeigr7@gmail.com>
2021-02-09 22:10:52 +0200

    mm

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <NavBar />
-      {Component.displayName === 'WithApollo(Home)' && <Hero />}
+      <Hero />
       <Component {...pageProps} />
       <Footer />
     </>
